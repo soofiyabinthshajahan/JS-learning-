f9634e4a752bbed66a73091aa716f5ec5c113513
soofiyabinthshajahan <soofiyashajahan@gmail.com>
2026-03-09 14:42:43 +0530

    task 1

---
+++ task1.js
@@ -0,0 +1,27 @@
+// TASK 1 — Predict ALL outputs before running a single line
+// Write your predictions as comments first, then verify
+
+console.log(typeof undefined)
+// undefined 
+console.log(typeof null)
+//null 
+console.log(typeof NaN)
+// number
+console.log(typeof function(){})
+// function
+console.log(null == undefined)
+// true 
+console.log(null === undefined)
+// false
+console.log(NaN === NaN)
+// false 
+
+console.log(1 + "2")
+// "12"
+console.log("3" - 1)
+//2
+console.log(true + true)
+// 2
+console.log([] + [])
+console.log([] + {})
+console.log({} + [])
